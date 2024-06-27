9b890aca1b5c60b19e898278657accc5e1dd810d
TioLindomar <mateusxavier0707@gmail.com>
2024-06-27 11:07:50 -0300

    Botão de voltar para a home no mobile

--- js/script.js
+++ js/script.js
@@ -19,3 +19,66 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+// Cadastro
+
+window.addEventListener('load', function() {
+    const form = document.getElementById('cadastroForm');
+    const cadastrarBtn = document.getElementById('cadastrarBtn');
+    const cancelarBtn = document.getElementById('cancelarBtn');
+    const resultadoDiv = document.getElementById('resultado');
+
+    cadastrarBtn.addEventListener('click', function() {
+        if (form.checkValidity()) {
+            const nomeCompleto = document.getElementById('nomeCompleto').value;
+            const telefone = document.getElementById('telefone').value;
+            const email = document.getElementById('email').value;
+            const dataNascimento = document.getElementById('dataNascimento').value;
+            const nomeUsuario = document.getElementById('nomeUsuario').value;
+            const senha = document.getElementById('senha').value;
+
+            resultadoDiv.innerHTML = `
+                <h2>Dados Cadastrados:</h2>
+                <p><strong>Nome Completo:</strong> ${nomeCompleto}</p>
+                <p><strong>Telefone:</strong> ${telefone}</p>
+                <p><strong>Email:</strong> ${email}</p>
+                <p><strong>Data de Nascimento:</strong> ${dataNascimento}</p>
+                <p><strong>Nome de Usuário:</strong> ${nomeUsuario}</p>
+                <p><strong>Senha:</strong> ${senha}</p>
+            `;
+        } else {
+            alert('Por favor, preencha todos os campos corretamente.');
+        }
+    });
+
+    cancelarBtn.addEventListener('click', function() {
+        form.reset();
+        resultadoDiv.innerHTML = '';
+    });
+});
+
+
+// Alert que leva a um link externo
+
+// Obtém o modal
+var mensagem = document.getElementById("minhaMensagem");
+
+// Obtém o <span> que fecha o modal
+var span = document.getElementsByClassName("fechar")[0];
+
+// Abre o modal ao carregar a página
+window.onload = function() { //  <--------- Uso de Onload
+    mensagem.style.display = "block";
+};
+
+// Fecha o modal quando o usuário clica no <span> (x)
+span.onclick = function() {
+    mensagem.style.display = "none";
+};
+
+// Fecha o modal quando o usuário clica fora do modal
+window.onclick = function(event) {
+    if (event.target == mensagem) {
+        mensagem.style.display = "none";
+    }
+};
